d59c386cd6fb2c525740a1f44dd604bbe89c4300
christophnetz <gandolf94@gmx.de>
2023-07-20 15:09:03 +0100

    initial commit
    Signed-off-by: christophnetz <gandolf94@gmx.de>

---
+++ DoL/Source.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <numeric>
+#include <fstream>
+#include <Eigen>
+
+#include "rndutils.hpp"
+
+using namespace std;
+using namespace Eigen;
+
+std::mt19937_64 rng;
+int Tmax = 100000;
+double lambda = 0.9; //forgetting, lim-> 10
+double mort = 0.006; //0.006
+double birthrate = 0.2; //0.2
+
+
+Matrix<double, 1, 3> suitability(const double size) {
+
+  Matrix<double, 1, 3> suit = { 
+    1.0 / (1.0 + exp((size - 15) / 4.0)), 
+    3.0 / ((1.0 + exp((25 - size) / 4.0)) * (1.0 + exp((size - 25) / 4.0))),
+    1.0 / (1.0 + exp((25 - size) / 4.0)) };
+  
+ 
+  return suit;
+}
+
+
+
+
+double sd(Eigen::ArrayXd vec) {
+  return std::sqrt((vec - vec.mean()).square().sum() / (vec.size()));
+}
+
+
+
+
+struct ind
+{
+  ind() : size(1.0), ID(1), updates(0) { 
+    m_experience = { 0.0, 0.0, 0.0 };
+    m_task = { 0.33, 0.33, 0.33 };
+  };
+
+
+  ind(int i) : size(1.0), updates(0), ID(i) { 
+    m_experience = { 0.0, 0.0, 0.0 }; 
+    m_task = { 0.33, 0.33, 0.33 };
+
+  };
+
+  double size;
+  int updates;
+  int ID;
+  Matrix<double, 1, 3> m_experience;
+  Matrix<double, 1, 3> m_task;
+
+  void experience() {
+
+    m_experience = m_task + lambda * m_experience;
+  }
+
+  void task( Matrix<double, 1, 3> labourdist) {
+
+    /*
+    get min of labour - pour in individual labour until equalized, distribute rest evenly
+    how much of their 'budget' follows intrinsic strength, how much follows balance requirement?
+
+    some scale may be needed, population labour vs individual contribution
+    skill = suitability(size, i) + m_experience[i]
+
+         
+
+    double time = 0.0;
+    2nd labourdist - min(labourdist) = x;
+    max(labourdist) - 2nd labourdist = y;
+
+    if (suitability(size, i) + m_experience[i]) < x
+      task[min] = 1, others 0;
+    else
+      task[min] = x / (suitability(size, i) + m_experience[i]);
+
+    if (suitability(size, i) + m_experience[i]) < 2 * y{
+      task[min] += ;
+    task[2nd] += ;
+    }
+    else {
+      task[min] += x / (suitability(size, i) + m_experience[i]);
+      task[2nd] += x / (suitability(size, i) + m_experience[i]);
+    }
+
+    for (int i = 0; i < m_task.size(); i++) {
+      task[i] += (suitability(size, i) + m_experience[i]);
+
+
+    }
+
+      
+ */
+
+
+    m_task = suitability(size) + m_experience - labourdist;
+    m_task.array() += 0.333;
+
+    updates++;
+    m_task = m_task / m_task.sum();
+  }
+
+  void labour(Matrix<double, 1, 3>& labour) {
+
+    labour += (m_task.array() * (suitability(size) + m_experience).array()).matrix();//*
+  }
+};
+
+double avg_size(std::vector<ind> v) {
+  double totalsize = 0;
+  for (int i = 0; i < v.size(); i++) {
+    totalsize += v[i].size;
+  }
+  return totalsize / static_cast<double>(v.size());
+}
+
+void deaths(vector<ind>& pop) {
+
+  std::binomial_distribution<int> d_dist(pop.size(), mort);
+  int deaths = d_dist(rng);
+  if (pop.size() - deaths < 5) {  //setting a minimal population size
+    deaths = pop.size() - 5;
+  }
+  if (deaths > 0) {
+
+    std::shuffle(std::begin(pop), std::end(pop), rng);
+    pop.resize(pop.size() - deaths);
+  }
+}
+
+void births(vector<ind>& pop, int& ID, const Matrix<double, 1, 3> labour) {
+  
+  int offspring = std::poisson_distribution<int> (birthrate)(rng);
+  for (int i = 0; i < offspring; ++i) {
+    pop.push_back(ind(ID));
+    pop.back().task(labour);
+    ID = (ID + 1);
+  }
+}
+
+
+
+
+int main() {
+
+  std::ofstream ofs1("output1.txt", std::ofstream::out);
+  std::ofstream ofs2("output2.txt", std::ofstream::out);
+  ofs1 << "t\tpopsize" << "\t" << "avgbodysize" << "\t" << "sdneed\teggcare\tdigging\tdefense" << std::endl;
+  ofs2 << "t\tID\tsize\tupdates\texp_egg\texp_digg\texp_def\ttask_egg\ttask_digg\ttask_def" << std::endl;
+
+  Matrix<double, 1, 3> labour = { 0.0, 0.0, 0.0 };
+
+  vector<ind> pop(30);
+  int ID = 0;
+
+  for (int i = 0; i < pop.size(); ++i) {
+    pop[i].size = static_cast<double>(i) + 1.0;
+  }
+
+  int next_t = 1;
+  // discrete times, but don't update simultaneously, instead draw individuals at random one after the other
+  for (double t = 0.0; t < Tmax; ) {
+
+    double delta_t = std::exponential_distribution<double>(static_cast<double>(pop.size()) * 0.2)(rng);
+
+    while (delta_t + t > static_cast<int>(next_t)) {
+      
+      births(pop, ID, labour);
+      deaths(pop);
+
+      labour = { 0.0, 0.0, 0.0 }; //reset labour vector
+
+      for (int i = 0; i < pop.size(); i++) {
+        pop[i].labour(labour);
+        pop[i].experience();
+        pop[i].size *= (1.0 + 0.2 * exp(-0.15 * pop[i].size));
+      }
+
+      labour = labour / labour.sum();
+
+
+      // Output
+      ofs1 << next_t << "\t" << pop.size() << "\t" << avg_size(pop) << "\t" << sd(labour.array()) << "\t" << labour << std::endl;
+
+      if (next_t > 90000.0 && next_t % 10 == 0) { //90000
+        for (int i = 0; i < pop.size(); ++i) {
+
+          ofs2 << next_t << "\t" << pop[i].ID << "\t" << pop[i].size << "\t" << pop[i].updates << "\t" << pop[i].m_experience << "\t" << pop[i].m_task << std::endl;
+
+        }
+      }
+      next_t++;
+    }
+
+
+    int sel_i = std::uniform_int_distribution<int>(0, pop.size() - 1) (rng);
+
+    pop[sel_i].task(labour);
+
+    t += delta_t;
+
+  }
+
+  return 0;
+}
+
+
---
+++ R/analysis.r
@@ -0,0 +1,139 @@
+library(reshape2)
+library(ggplot2)
+library(scales)
+library(readr)
+library(tidyverse)
+
+setwd("C:/Users/user/source/repos/DoL/DoL")
+
+
+data1 <- read.table("output1.txt", header = T)
+
+#population size dynamics
+ggplot(data1, aes(t, popsize))+ geom_line() 
+
+#average body size dynamics
+ggplot(data1, aes(t, avgbodysize))+ geom_line()
+
+#standard deviation need
+ggplot(data1, aes(t, sdneed))+ geom_line()
+
+#Tasks performed by group
+
+data1 <- gather(data1, eggcare, digging, defense, key = "behaviour", value = "prop")
+
+ggplot(data1, aes(t, prop, colour = behaviour))+ geom_line()+
+  geom_line(aes(t, avgbodysize/50), colour = "black")+xlim(99000, 100000)
+  
+
+#Individual-level data
+data2 <- read.table("output2.txt", header = T)
+
+data2 <- filter(data2, !ID %in% data2$ID[which(data2$t==90010)] )
+
+#To select data for 1000 individuals
+data2 <- filter(data2, ID %in% sample(unique(data2$ID), 1000))
+
+data2$suit_egg <- 1/(1 + exp((data2$size - 15)/4))
+data2$suit_digg <- 3/((1 + exp((25-data2$size)/4)) * (1 + exp((data2$size-25)/4)))
+data2$suit_def <- 1/(1 + exp((25-data2$size)/4))
+
+data2$labour_egg <- data2$task_egg * (data2$suit_egg + data2$exp_egg)
+data2$labour_digg <- data2$task_digg * (data2$suit_digg + data2$exp_digg)
+data2$labour_def <- data2$task_def * (data2$suit_def + data2$exp_def)
+
+#To just select 10000 different samples
+#data2 <- data2 %>% slice_sample(n = 10000)
+
+#To select data for 1000 individuals
+#data2 <- filter(data2, ID %in% sample(unique(data2$ID), 1000))
+
+
+lifehistory <- data2 %>% 
+  group_by(ID) %>% 
+  summarize(
+    maxage = max(t)-min(t),
+    birth = min(t),
+    maxsize = max(size)
+)
+
+data2 <- data2 %>% left_join(select(lifehistory, -maxsize, -maxage), by = "ID")
+data2$age <- data2$t - data2$birth
+
+#maximum age
+ggplot(lifehistory, aes(maxage))+geom_density(bounds = c(0, Inf))
+#maximum size
+ggplot(lifehistory, aes(maxsize))+geom_density(bounds = c(1, Inf))
+
+ggplot(data2, aes(size))+geom_density(bounds = c(1, Inf))
+ggplot(data2, aes(age))+geom_density(bounds = c(0, Inf))
+
+ggplot(data2, aes(age, size))+geom_point()
+
+#three different tasks over body size
+ggplot(data2, aes(size, task_egg))+geom_point()
+ggplot(data2, aes(size, task_digg))+geom_point()
+ggplot(data2, aes(size, task_def))+geom_point()
+
+
+ggplot(data2, aes(t, task_egg, colour = size))+geom_point()+xlim(99000, 100000)
+ggplot(data2, aes(t, exp_egg, colour = size))+geom_point()+xlim(99000, 100000)
+ggplot(data2, aes(t, task_egg, group = ID))+geom_line()+xlim(95000, 96000)
+
+ggplot(data2, aes(t, task_egg, colour = size, group = ID))+geom_point()+geom_line()+xlim(99000, 100000)
+
+
+ggplot(data2, aes(t, suit_egg, colour = size))+geom_point()+xlim(99000, 100000)
+#labour = task * (suitability + experience)
+
+groupbehaviour <- data2 %>% 
+  group_by(t) %>% 
+  summarize(
+    m_task_egg = mean(task_egg),
+    m_exp_egg = mean(exp_egg),
+    m_suit_egg = mean(suit_egg),
+    m_labour_egg = mean(labour_egg)
+  )
+
+groupmeasures <- gather(groupbehaviour, m_task_egg, m_exp_egg, m_suit_egg, m_labour_egg, key = "behaviour", value = "val")
+
+ggplot(groupmeasures, aes(t, val, colour = behaviour))+geom_line()+xlim(99000, 100000)
+
+
+
+
+
+
+
+library(plotly)
+
+plot_ly(
+  data2, a = ~task_egg, b = ~task_digg, c = ~task_def,
+  color = ~size,
+  type = "scatterternary")
+
+plot_ly(data2, x = ~task_egg, y = ~task_digg, z = ~task_def, color = ~size)
+
+ggplot(filter(data2, size > 40))+geom_point(aes(task_def, task_digg, color = size))
+ggplot(data2)+geom_point(aes(task_def, task_digg, color = size))
+
+ggplot(filter(data2, size > 40))+geom_point(aes(exp_def, exp_digg, color = size))
+
+
+ggplot(filter(data2, size > 40))+geom_point(aes(exp_def, exp_digg, color = size))
+
+filter(data2, size > 40, exp_digg< 1)
+
+filter(data2, size > 40, task_digg < 0.15)
+min(filter(data2, size > 40)$task_digg)
+# track single individuals
+
+ggplot(filter(data2, ID == 546522   ))+geom_point(aes(exp_def, exp_digg, color = size))
+ggplot(filter(data2, ID == 546522   ))+geom_point(aes(task_def, task_digg, color = size))
+ggplot(filter(data2, ID == 546522   ))+geom_point(aes(t, task_digg, color = size))
+ggplot(filter(data2, ID == 546522   ))+geom_point(aes(t, exp_digg, color = size))
+
+plot_ly(filter(data2, ID == 546522   ), x = ~task_egg, y = ~task_digg, z = ~task_def, color = ~size, size = 0.1)
+
+
+
